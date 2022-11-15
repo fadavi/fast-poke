7a3c4469d1ccb43750a3bfaef0e35848f760d04e
Mohamad Fadavi <fadavimohamad@gmail.com>
2022-11-15 22:06:35 +0300

    ref: format

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,24 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance } from "fastify";
 import { getPokemonByNameOrID } from "./handlers";
 
 export default async function router(fastify: FastifyInstance) {
-  fastify.get("/poke/:nameOrID", {
-    schema: {
-      params: {
-        type: "object",
-        properties: {
-          nameOrID: {
-            type: "string",
-            minLength: 1,
-            maxLength: 64,
-            pattern: "^[0-9A-Za-z ]+$",
+  fastify.get(
+    "/poke/:nameOrID",
+    {
+      schema: {
+        params: {
+          type: "object",
+          properties: {
+            nameOrID: {
+              type: "string",
+              minLength: 1,
+              maxLength: 64,
+              pattern: "^[0-9A-Za-z ]+$",
+            },
           },
         },
       },
     },
-  }, getPokemonByNameOrID);
+    getPokemonByNameOrID
+  );
 }
